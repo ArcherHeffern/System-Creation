47a48f882f2973053250cb959e61b59a8e5aff28
Archer Heffern <heffernarcher@gmail.com>
2024-03-03 01:33:17 -0500

    updated error handling and whatnot in tamidcat

--- exercises/L5/tamidcat.c
+++ exercises/L5/tamidcat.c
@@ -6,11 +6,13 @@
 #include <stdlib.h>
 
 #define ERR_NOT_ENOUGH_ARGUMENTS 1
+#define ERR_READ 2
+#define ERR_CLOSE 3
 #define BUFSIZE 1024
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-        printf("Not enough arguments\n");
+        fprintf(stderr, "Not enough arguments\n");
         exit(ERR_NOT_ENOUGH_ARGUMENTS);
     }
     int f = open(argv[1], O_RDONLY);
@@ -18,10 +20,15 @@ int main(int argc, char** argv) {
     char buf[BUFSIZE];
     while (n > 0) {
         n = read(f, buf, BUFSIZE - 1);
+        if (n < 0) {
+            perror("Read");
+            exit(ERR_READ);
+        }
         buf[n] = 0;
-        write(1, buf, BUFSIZE);
+        write(1, buf, BUFSIZE); // My goodness, what does this mean? 
     }
-    if (n < 0) {
-        fprintf(stderr, "Erorr reading file\n");
+    if (close(f) == -1) {
+        perror("Close");
+        exit(ERR_CLOSE);
     }
 }
