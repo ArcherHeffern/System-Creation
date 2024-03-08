8be511cedd1c3452c20aef050a9589fdade23309
Archer Heffern <heffernarcher@gmail.com>
2024-03-07 20:49:20 -0500

    added fork_bug description of expected output

--- exercises/L2/fork_bug.py
+++ exercises/L2/fork_bug.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 from os import fork
 
+"""
+Expected output: 
+"Hello from child!" and "Hello from parent!" printed 10 times each
+"""
+
 for i in range(10):
     if fork() == 0:
         print("Hello from child!")
