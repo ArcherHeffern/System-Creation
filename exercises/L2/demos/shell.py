0542c95a64acb2c0273268121e7a481b80d3a325
Archer Heffern <heffernarcher@gmail.com>
2024-03-10 17:13:44 -0400

    'updated_shell'

--- exercises/L2/demos/shell.py
+++ exercises/L2/demos/shell.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
 
-from os import fork, wait, execvp
+from os import fork, wait, execvp, chdir
 
 while True:
-    commands = input("> ").strip().split()
+    commands: list[str] = input("> ").strip().split()
     if len(commands) == 0:
         continue
     
-    command = commands[0]
+    command: str = commands[0]
 
+    # Process any builtins
     if command == 'exit':
         exit(0)
+    if command == 'cd':
+        if len(commands) == 2:
+            chdir(commands[1])
+        continue
 
+    # If no builtins, execute command in a new process
     if fork() == 0:
         try:
             execvp(command, commands)
         except:
             print("Invalid command [%s]" % command)
-        exit(1)
+            exit(1)
     wait()
