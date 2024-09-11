025850f45665c7018356f77cfcbb8440351f0d75
ArcherHeffern <heffernarcher@gmail.com>
2024-09-11 19:28:42 -0400

    update naming of shell args for clarity

--- archive3/exercises/L2/demos/shell.py
+++ archive3/exercises/L2/demos/shell.py
@@ -10,25 +10,26 @@ CHANGE_DIRECTORY = chdir
 
 
 while True:
-    commands: list[str] = input("> ").strip().split()
-    if len(commands) == 0:
+    tokens: list[str] = input("> ").strip().split()
+    if len(tokens) == 0:
         continue
     
-    command: str = commands[0]
+    command: str = tokens[0]
+    args = tokens
 
     # Process any builtins
     match command:
         case 'exit':
             exit(0)
         case 'cd':
-            if len(commands) == 2:
-                CHANGE_DIRECTORY(commands[1])
+            if len(args) == 2:
+                CHANGE_DIRECTORY(args[1])
             continue
 
     # If no builtins, execute command in a new process
     if CREATE_NEW_PROCESS() == 0:
         try:
-            RUN_PROGRAM(command, commands)
+            RUN_PROGRAM(command, args)
         except:
             print("Invalid command [%s]" % command)
             exit(1)
