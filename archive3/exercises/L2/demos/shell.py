b7c8020a179129317e9822710947add11076fac7
ArcherHeffern <heffernarcher@gmail.com>
2024-09-11 16:46:37 -0400

    update shell for clarity

--- archive3/exercises/L2/demos/shell.py
+++ archive3/exercises/L2/demos/shell.py
@@ -3,6 +3,12 @@
 # These are special functions! They are system calls!	
 from os import fork, wait, execvp, chdir
 
+RUN_PROGRAM = execvp
+CREATE_NEW_PROCESS = fork
+WAIT_FOR_RUNNING_PROGRAM_TO_FINISH = wait
+CHANGE_DIRECTORY = chdir
+
+
 while True:
     commands: list[str] = input("> ").strip().split()
     if len(commands) == 0:
@@ -11,19 +17,19 @@ while True:
     command: str = commands[0]
 
     # Process any builtins
-	match command:
-		case 'exit':
-			exit(0)
-		case 'cd':
-			if len(commands) == 2:
-				chdir(commands[1])
-			continue
+    match command:
+        case 'exit':
+            exit(0)
+        case 'cd':
+            if len(commands) == 2:
+                CHANGE_DIRECTORY(commands[1])
+            continue
 
     # If no builtins, execute command in a new process
-    if fork() == 0:
+    if CREATE_NEW_PROCESS() == 0:
         try:
-            execvp(command, commands)
+            RUN_PROGRAM(command, commands)
         except:
             print("Invalid command [%s]" % command)
             exit(1)
-    wait()
+    WAIT_FOR_RUNNING_PROGRAM_TO_FINISH()
